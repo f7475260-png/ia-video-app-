d4b9a98e7405792bdc9b09964dd778be36ab8138
f7475260-png <f7475260@gmail.com>
2025-08-14 13:39:47 +0200

    Delete utils/music_selector.py

--- utils/music_selector.py
+++
@@ -1,9 +0,0 @@
-import os
-import random
-
-def get_music(format_type: str = "youtube") -> str | None:
-    music_dir = "assets/music"
-    if not os.path.isdir(music_dir):
-        return None
-    files = [os.path.join(music_dir, f) for f in os.listdir(music_dir) if f.lower().endswith(".mp3")]
-    return random.choice(files) if files else None
